976c97a348930cb5ae34add8cfc63f9751feaee7
Gian <hunoldgian@outlook.de>
2024-05-01 16:18:07 +0200

    Knockback now with config file

--- knockback/src/main/java/org/gian/knockback/Knockback.java
+++ knockback/src/main/java/org/gian/knockback/Knockback.java
@@ -1,16 +1,34 @@
 package org.gian.knockback;
 
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
+import org.bukkit.*;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.gian.knockback.knockback.commands.knockback;
 import org.gian.knockback.knockback.events.knockoff;
 
 public final class Knockback extends JavaPlugin {
 
+    public static String knockbackitem;
+    public static int enchantlevel;
+
     @Override
     public void onEnable() {
+        //Check for Config File | If not there, Create config File
+        if (!getDataFolder().exists()){
+            getLogger().info("Creating "+ getDataFolder() +" main directory ");
+            getDataFolder().mkdir();
+            //creating main plugin directory if not exists
+            getLogger().info("Creating config file for: " + getDataFolder());
+            this.getConfig().set("Knockback Item", "STICK");
+            this.getConfig().set("Knockback Strenght", 7);
+            this.saveConfig();
+            // then saving the config
+        }
+
+        knockbackitem = this.getConfig().getString("Knockback Item");
+        enchantlevel = this.getConfig().getInt("Knockback Strenght");
+
         // Plugin startup logic
 
         //Generate Worlds
--- knockback/src/main/java/org/gian/knockback/knockback/commands/knockback.java
+++ knockback/src/main/java/org/gian/knockback/knockback/commands/knockback.java
@@ -7,15 +7,22 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.gian.knockback.Knockback;
+
+import java.util.Objects;
 
 import static org.bukkit.Bukkit.getServer;
 
 public class knockback implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+
         Player player = (Player) commandSender;
         Location knockbackmid = new Location(getServer().getWorld("knockbackmap"), 0.5,101,0.5);
 
@@ -25,11 +32,11 @@ public class knockback implements CommandExecutor {
         player.setFoodLevel(20);
 
         player.getInventory().setHeldItemSlot(0);
-        player.getInventory().setItemInMainHand(new ItemStack(Material.STICK,1));
-
+        //player.getInventory().setItemInMainHand(new ItemStack(Material.STICK,1));
+        player.getInventory().setItemInMainHand(new ItemStack(Material.getMaterial(Knockback.knockbackitem),1));
         ItemStack currentitem = player.getInventory().getItemInMainHand();
         ItemMeta meta = currentitem.getItemMeta();
-        meta.addEnchant(Enchantment.KNOCKBACK, 7, true);
+        meta.addEnchant(Enchantment.KNOCKBACK, Knockback.enchantlevel, true);
         currentitem.setItemMeta(meta);
 
         player.setGameMode(GameMode.SURVIVAL);
